e7fe6cab5893769adf7aec10da8c7c4fa664e700
Ozone Bhattarai <ozonebhattarai@gmail.com>
2023-07-01 19:13:02 +0545

    create initial root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import Providers from "@/utils/provider";
+import ReactQueryProvider from "@/utils/provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,9 +18,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Providers>
-        <body className={inter.className}>{children}</body>
-      </Providers>
+      <ReactQueryProvider>
+        <body className={inter.className}>
+          <nav className="sticky h-20 flex items-center justify-between top-0 left-0 px-5 shadow-md">
+            <Image src="/logo.svg" alt="logo" width={130} height={100} />
+            <Link className="text-yellow-700 font-bold" href="/search">
+              Search
+            </Link>
+          </nav>
+
+          {children}
+        </body>
+      </ReactQueryProvider>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <div>
-      <h1>Music Sansar</h1>
-      <p>Sansar for music lovers</p>
+      <Head>
+        <title>Music Sansar</title>
+      </Head>
+      <p>Hello</p>
     </div>
   );
 }
