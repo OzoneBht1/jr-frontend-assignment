df44cfaafba9012c7c952ea87f28a7d30e31491f
Ozone Bhattarai <ozonebhattarai@gmail.com>
2023-07-03 04:24:38 +0545

    add api usage explanation

--- queryFns/getTrendingAlbums.ts
+++ queryFns/getTrendingAlbums.ts
@@ -5,6 +5,7 @@ export const getTrendingAlbums = async (accessToken?: string) => {
     params.append("limit", "20");
     params.append("skip", "0");
     const response = await fetch(
+      // This endpoint gets top 50 tracks from an auto updated playlist on Spotify, which is then used for getting the albums
       `https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?${params.toString()}`,
       {
         method: "GET",
