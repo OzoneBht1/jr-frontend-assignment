3e73bbbbf6b86cb8abb963b7c56896124665247d
Ozone Bhattarai <ozonebhattarai@gmail.com>
2023-07-02 13:41:42 +0545

    add trending album endpoint

---
+++ queryFns/getTrendingAlbums.ts
@@ -0,0 +1,26 @@
+export const getTrendingAlbums = async (accessToken?: string) => {
+  try {
+    console.log("hi mom ");
+    const params = new URLSearchParams();
+    console.log(accessToken);
+    params.append("limit", "20");
+    params.append("skip", "0");
+    const response = await fetch(
+      `https://api.spotify.com/v1/playlists/37i9dQZEVXbMDoHDwVN2tF/tracks?${params.toString()}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "Application/json",
+          Authorization: "Bearer " + accessToken,
+        },
+      }
+    );
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    return response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
